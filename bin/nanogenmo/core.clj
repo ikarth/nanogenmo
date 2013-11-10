0d4b280e578c45f9c7aeefb267b830aa62e6799c
IJK <isaac@isaackarth.com>
2013-11-09 21:16:44 -0500

    Baby steps

--- bin/nanogenmo/core.clj
+++ bin/nanogenmo/core.clj
@@ -2,7 +2,12 @@
   (:require [clojure.pprint]
             [opennlp.nlp]
             [opennlp.treebank])
+  (:use [clojure.pprint]
+        [opennlp.nlp]
+        [opennlp.treebank])
   (:gen-class))
+
+(use 'clojure.java.io)
 
 (def get-sentences (make-sentence-detector "models/en-sent.bin"))
 (def tokenize (make-tokenizer "models/en-token.bin"))
@@ -16,3 +21,41 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+;(defn input-source-text [source-text]
+;  (with-open [rdr (reader source-text)]
+;    (doseq [line (line-seq rdr)]
+;      (println line))))
+
+(defn strip-linebreaks [text]
+  "Returns string with linebreaks stripped out."
+  (clojure.string/replace text "\r\n" " "))
+
+;(defn input-source-text-2 [source-text]
+;  (get-sentences (slurp source-text)))
+
+(defn mark-paragraphs [source-text]
+  (clojure.string/replace source-text #"\r\n\r\n" "¶"))
+
+(defn get-paragraphs [source-text]
+  "Returns source-texts broken into a list of paragraphs."
+  (clojure.string/split source-text #"¶"))
+
+(defn categorize-paragraph [source-text]
+  "Returns a category based on the contents of the source-text: dialog, action, or exposition."
+  ;(clojure.string/
+  )
+
+(defn input-source-text [source-text]
+  "Takes cleaned source text and formats it into useful paragraphs."
+  (get-paragraphs (strip-linebreaks 
+                    (mark-paragraphs 
+                      (slurp source-text)))))
+
+(input-source-text
+  "texts\\cleaned\\pnp_excerpt.txt")
+
+
+;(get-sentences (strip-linebreaks
+;(get-paragraphs (strip-linebreaks (mark-paragraphs (slurp "texts\\cleaned\\pnp_excerpt.txt"))))
+
